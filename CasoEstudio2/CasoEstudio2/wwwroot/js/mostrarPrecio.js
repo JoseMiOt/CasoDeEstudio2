b935e5cdb719060dc60bb584e2eacc8e04871e2f
JoseMiOt <josmiranda32@gmail.com>
2023-08-19 03:38:49 -0600

    Caso Finalizado

---
+++ CasoEstudio2/CasoEstudio2/wwwroot/js/mostrarPrecio.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var idCasaDropdown = document.getElementById("IdCasa");
+    var casaIdHidden = document.getElementById("casaIdHidden");
+    var precioCasaInput = document.getElementById("PrecioCasa");
+
+    idCasaDropdown.addEventListener("change", function () {
+        var selectedOption = idCasaDropdown.options[idCasaDropdown.selectedIndex];
+        var selectedId = selectedOption.value;
+        var selectedPrecio = selectedOption.getAttribute("data-precio");
+
+        casaIdHidden.value = selectedId;
+        precioCasaInput.value = selectedPrecio; // Actualiza el valor del campo PrecioCasa
+    });
+});
---
+++ CasoEstudio2/CasoEstudio2/wwwroot/js/obtenerId.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var idCasaDropdown = document.getElementById("IdCasa");
+    var casaIdHidden = document.getElementById("casaIdHidden");
+
+    idCasaDropdown.addEventListener("change", function () {
+        var selectedId = idCasaDropdown.value;
+        casaIdHidden.value = selectedId;
+    });
+});
